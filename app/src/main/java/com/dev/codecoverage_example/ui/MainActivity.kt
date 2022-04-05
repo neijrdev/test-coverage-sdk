d129fef21697b8a2430abba2b0f31399ca08fd0a
nei.jr.dev <n.junior@paylivre.com>
2022-04-05 17:57:07 -0300

    first commit

---
+++ app/src/main/java/com/dev/codecoverage_example/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.dev.codecoverage_example.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.codecoverage_example.databinding.ActivityMainBinding
+import com.dev.codecoverage_example.util.TextMethods
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.save.setOnClickListener {
+            //   showt.text = TextMethods.counttext(addtitle.text.toString(), adddescription.text.toString()).toString()
+
+            binding.showt.text =
+                TextMethods.capitalizeText(
+                    binding.addtitle.text.toString(),
+                    binding.adddescription.text.toString()
+                )
+
+        }
+    }
+
+}
---
+++ app/src/test/java/com/dev/codecoverage_example/util/TextMethodsTest.kt
@@ -0,0 +1,24 @@
+package com.dev.codecoverage_example.util
+
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+
+
+class TextMethodsTest {
+
+    @Test
+    fun counttext() {
+        val yu = TextMethods.counttext("tuuuuu", "")
+        // assertTrue("text contains two characters", yu == 2)
+        //  assertNotNull("text is not null", yu)
+        assertThat(yu).isGreaterThan(4)
+
+    }
+
+    @Test
+    fun capitalizeText() {
+        val yu = TextMethods.capitalizeText("tuuuuu", "")
+        assertThat(yu).contains("TUUUUU")
+    }
+
+}
